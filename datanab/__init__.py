1093168a022f0324377b2710802d1073e5f0f5e5
jmfernandes <joshfernandes@mac.com>
2018-10-24 23:04:55 -0700

    added docs

--- datanab/__init__.py
+++ datanab/__init__.py
@@ -6,13 +6,18 @@ class dotdict(dict):
 
 #Constants
 planck_constant = dotdict(r.get("http://constants.datanab.net/physics/planck_constant_json").json())
+"""int: Module level variable documented inline.
 
-standard_gravity = dotdict(r.get("https://constants.datanab.net/physics/standard_gravity_json").json())
+The docstring may span multiple lines. The type may optionally be specified
+on the first line, separated by a colon.
+"""
 
-electron_mass = dotdict(r.get("https://constants.datanab.net/physics/electron_mass_json").json())
+standard_gravity = dotdict(r.get("http://constants.datanab.net/physics/standard_gravity_json").json())
 
-vacuum_impedance = dotdict(r.get("https://constants.datanab.net/physics/characteristic_impedance_of_vacuum_json").json())
+electron_mass = dotdict(r.get("http://constants.datanab.net/physics/electron_mass_json").json())
 
-vacuum_permittivity = dotdict(r.get("https://constants.datanab.net/physics/vacuum_permittivity_json").json())
+vacuum_impedance = dotdict(r.get("http://constants.datanab.net/physics/characteristic_impedance_of_vacuum_json").json())
 
-vacuum_permeability = dotdict(r.get("https://constants.datanab.net/physics/vacuum_permeability_json").json())
+vacuum_permittivity = dotdict(r.get("http://constants.datanab.net/physics/vacuum_permittivity_json").json())
+
+vacuum_permeability = dotdict(r.get("http://constants.datanab.net/physics/vacuum_permeability_json").json())
